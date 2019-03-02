8413ce5d008067c4d14725e3857d47775ba9487a
davidwkaiser <davidwkaiser@yahoo.com>
2019-03-01 22:42:58 -0600

    get basic login working with in memory user

--- src/main/java/com/project/base/config/BasicConfiguration.java
+++ src/main/java/com/project/base/config/BasicConfiguration.java
@@ -6,32 +6,42 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class BasicConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-        .inMemoryAuthentication()
-        .withUser("user")
-        .password("password1")
-        .roles("USER")
-        .and()
-        .withUser("admin")
-        .password("admin1")
-        .roles("USER", "ADMIN");
+                .inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("user")
+                .password("password1")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("admin1")
+                .roles("USER", "ADMIN");
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
+    protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
+            .authorizeRequests()
+                .antMatchers("/resources/**")
+                .permitAll()
+                .antMatchers("first").hasRole("USER")
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
 
     }
 
---
+++ src/main/java/com/project/base/controllers/LoginController.java
@@ -0,0 +1,21 @@
+package com.project.base.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login(Model model){
+        return "login";
+    }
+
+
+    @RequestMapping("/first")
+    public String getFirst(Model model){
+        return "first";
+    }
+
+}
