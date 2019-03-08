34e82c0d95eb785f879f15a84aa709eea8dfbcff
davidwkaiser <davidwkaiser@yahoo.com>
2019-03-08 06:50:06 -0600

    add user service

---
+++ src/main/java/com/project/base/services/UserService.java
@@ -0,0 +1,33 @@
+package com.project.base.services;
+
+import com.project.base.objects.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements IUserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Transactional
+    @Override
+    public User registerNewUserAccount(UserDto accountDto)
+        throws EmailExistsException{
+        if (emailExist(accountDto.getEmail())){
+            throw new EmailExistsException(
+                    "There is an account with that email address: " +accountDto.getEmail();
+            )
+        }
+
+    }
+    private boolean emailExists(String email){
+        User user = repository.findByEmail(email);
+        if (user != null) {
+            return true;
+        }
+        return false;
+    }
+
+}
